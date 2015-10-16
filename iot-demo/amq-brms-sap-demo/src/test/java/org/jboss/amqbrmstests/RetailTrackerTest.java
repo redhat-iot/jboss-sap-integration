2bdc88376596d640d6aaf1a8c6b3531bbc5acd85
Krishnan Prasanna Sivarama <kprasann@kprasann-OSX.local>
2015-10-16 10:59:31 -0700

    adding amq brms code

---
+++ iot-demo/amq-brms-sap-demo/src/test/java/org/jboss/amqbrmstests/RetailTrackerTest.java
@@ -0,0 +1,95 @@
+package org.jboss.amqbrmstests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+
+import javax.inject.Inject;
+
+import org.jboss.brmspojos.CustomerObj;
+import org.jboss.brmspojos.Department;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.kie.api.KieBase;
+import org.kie.api.KieBaseConfiguration;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.Message;
+import org.kie.api.builder.Results;
+import org.kie.api.cdi.KSession;
+import org.kie.api.conf.EventProcessingOption;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.api.runtime.StatelessKieSession;
+import org.kie.api.runtime.rule.EntryPoint;
+
+/**
+ * Test Cases for SAP Red Hat Demo
+ * @author kprasann
+ *
+ */
+public class RetailTrackerTest {
+	
+	//@Inject
+	//   @KSession("myKSessionTest")
+	    // use configured myKSession
+	 //   private KieSession kieSession;
+	private static KieSession kSession;
+	
+	private static String[] specialDepts = new String []{"PHARMACY","FOOD","APPAREL"};
+		
+	 @BeforeClass
+	    public static void setup() throws FileNotFoundException {
+	     
+		    KieServices ks = KieServices.Factory.get();
+		 
+		    KieContainer kieContainer = ks.getKieClasspathContainer();
+
+	        KieBaseConfiguration config = ks.newKieBaseConfiguration();
+	        config.setOption( EventProcessingOption.STREAM );
+	        KieBase kieBase = kieContainer.newKieBase( config );
+	        //      KieSession kieSession = kieContainer.newKieSession();
+	        kSession = kieBase.newKieSession();
+	        kSession.setGlobal("specialDeptList", Arrays.asList(specialDepts));
+	    }
+	 
+	@Test
+    public void testCustomerInDepartment() {
+        System.out.println("** Check if customer is in a department with 3 moves and designate as entered **");
+        
+        Point custPt = new Point(2,2);
+        Point custPt1 = new Point(3,3);
+        Rectangle deptLocation = new Rectangle(1,2,2,4);
+        
+        //Department dept = new Department(1, "PHARMACY", deptLocation);
+        Department dept = new Department("PHARMACY", deptLocation);
+
+	   // CustomerObj c = new CustomerObj("122ATP1212", 1223323, 0, custPt, dept);
+	    CustomerObj c = new CustomerObj("122ATP1212", custPt);
+	    CustomerObj c1 = new CustomerObj("A122ATP1212", custPt1);
+	    CustomerObj c3 = new CustomerObj("122ATP1212BB", custPt);
+	    CustomerObj c4 = new CustomerObj("A122ATP1212ff", custPt1);
+	    CustomerObj c5 = new CustomerObj("122ATP1212", custPt);
+
+
+
+	    kSession.insert(c);
+	    kSession.insert(c1);
+	    kSession.insert(c3);
+	    kSession.insert(c4);
+	    kSession.insert(c5);
+	    kSession.insert(dept);
+        kSession.fireAllRules();
+        System.out.println("* Transaction Completed: ");
+    }
+
+
+}
